97fc2d46ca418decf07e60170c96b7bec14edd7c
penggy <peng.wu@foxmail.com>
2016-11-04 12:16:01 +0800

    fill code

--- app.js
+++ app.js
@@ -1,52 +1,5 @@
 ﻿var env = process.env.NODE_ENV || 'development';
 global.db = require("./db");
-global.page = function (req, res, sql, params) {
-	var page = parseInt(req.body.page);
-	var limit = parseInt(req.body.rows);
-	if (isNaN(page) || isNaN(limit)) {
-		res.status(500).send("分页数据缺失");
-		return;
-	}
-	var start = limit * (page - 1);
-	var _sql = sql.replace(/\s*(o|O)(r|R)(d|D)(e|E)(r|R)\s+(b|B)(y|Y).*$/, "");
-	cntSql = "select count(*) from (" + _sql + ") as count";
-	var ret = {};
-	var db = new Database;
-	try {
-		db.serialize(function () {
-			db.get(cntSql, params, function (err, row) {
-				if (err) throw err;
-				ret.total = row["count"];
-			});
-			if (req.body.sort) {
-				_sql += " order by " + req.body.sort + " " + req.body.order;
-			}
-			_sql += " limit ?,?";
-			params.push(start, limit);
-			db.all(_sql, params, function (err, rows) {
-				if (err) throw err;
-				ret.rows = rows;
-			})
-			res.json(ret);
-		})
-	} catch (e) {
-		res.status(500).send(err.message);
-	} finally {
-		db.close();
-	}
-}
-global.onError = function (err, req, res) {
-	var fs = require('fs');
-	var errorLogfile = fs.createWriteStream('error.log', { flags: 'a' });
-	errorLogfile.write('[' + new Date() + '] ' + req.url + '\n');
-	errorLogfile.write(err.stack + '\n');
-	if (req.xhr) {
-		res.status(500).send(err.message);
-	} else {
-		res.render('error', { title: "出错了", message: err.message });
-	}
-}
-
 var express = require('express');
 var bodyParser = require('body-parser')
 var routes = require(__dirname + '/routes');
@@ -144,7 +97,15 @@ app.post('/user/save', routes.user.save);
 app.post('/user/remove', routes.user.remove);
 
 app.use(function (err, req, res, next) {
-	onError(err,req,res);
+	var fs = require('fs');
+	var errorLogfile = fs.createWriteStream('error.log', { flags: 'a' });
+	errorLogfile.write('[' + new Date() + '] ' + req.url + '\n');
+	errorLogfile.write(err.stack + '\n');
+	if (req.xhr) {
+		res.status(500).send(err.message);
+	} else {
+		res.render('error', { title: "出错了", message: err.message });
+	}
 });
 
 if (!module.parent) {
--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 var sqlite3 = require('sqlite3').verbose();
+var co = require('co');
 var Promise = require('promise');
 
 function Database(){
@@ -20,4 +21,79 @@ exports.get = function(sql,params){
             })
         }
     });
+}
+
+exports.all = function(sql,params){
+    return Promise.resolve({
+        then : function(onFulfill,onReject){
+            var db = new Database();
+            db.all(sql,params,function(err,rows){
+                if(err){
+                    db.close();
+                    onReject(err);
+                    return;
+                }
+                onFulfill(rows);
+                db.close();
+            })
+        }
+    });
+}
+
+exports.run = function(sql,params){
+    return Promise.resolve({
+        then : function(onFulfill,onReject){
+            var db = new Database();
+            db.run(sql,params,function(err){
+                if(err){
+                    db.close();
+                    onReject(err);
+                    return;
+                }
+                onFulfill(this);
+                db.close();
+            })
+        }
+    });
+}
+
+exports.exec = function(sql,params){
+    return Promise.resolve({
+        then : function(onFulfill,onReject){
+            var db = new Database();
+            db.exec(sql,params,function(err){
+                if(err){
+                    db.close();
+                    onReject(err);
+                    return;
+                }
+                onFulfill(this);
+                db.close();
+            })
+        }
+    });
+}
+
+exports.page = function (req, res, sql, params) {
+    co(function*(){
+        var page = parseInt(req.body.page);
+        var limit = parseInt(req.body.rows);
+        if (isNaN(page) || isNaN(limit)) throw new Error("分页数据缺失");
+        var start = limit * (page - 1);
+        var _sql = sql.replace(/\s*(o|O)(r|R)(d|D)(e|E)(r|R)\s+(b|B)(y|Y).*$/, "");
+        cntSql = "select count(*) from (" + _sql + ") as count";
+        var ret = {};
+        var row = yield exports.get(cntSql,params);
+        ret.total = row["count"];
+        if (req.body.sort) {
+            _sql += " order by " + req.body.sort + " " + req.body.order;
+        }
+        _sql += " limit ?,?";
+        params.push(start, limit);
+        var rows = yield exports.all(_sql,params);
+        ret.rows = rows;
+        res.json(ret);
+    }).catch(function(err){
+        res.status(500).send(err.message);
+    })
 }
